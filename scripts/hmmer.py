53b889a6cc32900cfd15bb4d599675a84f1f4ab0
zeynepkurtw <zeynepkurt.w@gmail.com>
2022-11-28 16:30:43 +0100

    TE

--- scripts/hmmer.py
+++ scripts/hmmer.py
@@ -2,10 +2,10 @@ from snakemake.shell import shell
 
 #align = snakemake.input.align
 align = snakemake.input.align
-db= snakemake.input.db
+#db= snakemake.input.db
 
 hmm = snakemake.output[0]
-out = snakemake.output[1]
+#out = snakemake.output[1]
 
 #shell(f"""hmmbuild {hmm} {align}""")
 shell(f"""hmmbuild {hmm} {align}""")
